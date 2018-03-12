eec54f7937f727068422f628b35e6d02d0671633
unknown <maureenicolecalpito17@gmail.com>
2018-03-12 18:36:13 +0800

    Added print.php

---
+++ old/pages/print.php
@@ -0,0 +1,22 @@
+<?php
+echo "<h3><b>WiFiRA Wireless Voucher</b></h3>";
+echo "<ul><u><b>Valid for 2 hrs</u></b></ul>";
+echo "<ul><b>10007-00000</b></ul>";
+echo "<p>___________________________<p>";
+echo "<h3><b>WiFiRA Wireless Voucher</b></h3>";
+echo "<ul><u><b>Valid for 2 hrs</u></b></ul>";
+echo "<ul><b>10011-00000</b></ul>";
+echo "<p>___________________________<p>";
+echo "<h3><b>WiFiRA Wireless Voucher</b></h3>";
+echo "<ul><u><b>Valid for 2 hrs</u></b></ul>";
+echo "<ul><b>10012-00000</b></ul>";
+echo "<p>___________________________<p>";
+echo "<h3><b>WiFiRA Wireless Voucher</b></h3>";
+echo "<ul><u><b>Valid for 2 hrs</u></b></ul>";
+echo "<ul><b>10015-00000</b></ul>";
+echo "<p>___________________________<p>";
+echo "<h3><b>WiFiRA Wireless Voucher</b></h3>";
+echo "<ul><u><b>Valid for 2 hrs</u></b></ul>";
+echo "<ul><b>10016-00000</b></ul>";
+echo "<p>___________________________<p>";
+?>
