57739b82b7c0f414b7bd711c9a5037a7ee1b53a9
turquezakath <turquezakath@gmail.com>
2018-03-14 15:54:48 +0800

    Modified log in page.

--- old/login.php
+++ old/login.php
@@ -13,27 +13,34 @@ require 'classes/UserAccount.php';
 
     
 </head>
-<body background="pages/assets/img/background1.jpg" id="bg" >
+<body id="bg" >
   
-  <div class="backimage fright" align="center">
+  <div align="center">
   <img src="pages/assets/img/logo.png" alt="Avatar" class="image1">
-  <div class="overlay">Wireless Internet Service Provider</div>
   </div>
-  <div class="fleft" align="left">
-    <div id="loginborder" align="left">
+  <div align="center">
+    <div id="loginborder" align="center">
       <?php
         if(isset($errMsg)){
           echo '<div style="color:black;text-align:center;font-size:120px;">'.$errMsg.'</div>';
         }
       ?>
-      <div class="headergreen"><b>WiFiRA Login</b></div>
+      <div class="headergreen"><b>Login</b></div>
       <div style="margin:30px">
           <form action="" method="post">
-          <label class="userin"><b> Username: </b></label><input type="text" name="username" class="box"/><br /><br />
-          <label class="userin"><b>Password:</b></label><input type="password" name="password" class="box" /><br/><br />
-          <input type="submit" name='submit' class="btn btn-warning" value="Submit" class="col s6" class='submit' id="submit1" /><br />
+        <div class="form-group">
+            <div class="col-lg-10">
+            <input type="text" class="form-control" name="username" placeholder="Username"><br/><br />
+            </div>
+        </div>  
+        <div class="form-group">
+            <div class="col-lg-10">
+            <input type="password" class="form-control" name="password" placeholder="Password"><br/><br />
+            </div>
+        </div>  
+         
+          <input type="submit" name='submit' class="btn btn-warning" value="Submit" class="col s6" class='submit'/><br />
           </form>
-          
       </div>
     </div>
   </div>
