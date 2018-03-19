8020240e6193612c8eba0b88306d1630f137e8cf
leizel199 <alfa.leones199@gmail.com>
2018-03-19 07:26:36 +0200

    Updated vouchers-unsold.php Filter Status

--- old/pages/vouchers-unsold.php
+++ old/pages/vouchers-unsold.php
@@ -39,9 +39,24 @@ require '../classes/UserAccount.php';
                     <div class="col-md-12">
                         <h1 style = "font-family: special elite; color:#000000">Vouchers</h1>
 
-                    
+						<form id="search-form" name="search" action="vouchers-entity.php" method="get">
+    							<select name = "entity">
+    								<option value="">Choose Status</option>
+    								<?php 
+                                		require_once 'fragments/connection.php';
+                                		$usersQuerry = $pdo->prepare("SELECT DISTINCT voucherstatus FROM vouchers; ");
+                                		$usersQuerry->execute();
+                                		$users = $usersQuerry->fetchAll();
 
-
+                                		foreach ($users as $user){
+                                			echo "<option>" . $user['voucherstatus'] . "</option>";
+                             			}
+                            		?>
+    							</select>
+    						
+                                <input type="submit" name='submit' class="btn btn-warning" value="Search" class="col s6" class='submit' style="background-color:#686667; font-family:monospace; font-size:18px;"/>
+                            </form>	
+							
                     </div>    
                 </div>
 
@@ -75,30 +90,6 @@ require '../classes/UserAccount.php';
                         <?php
                          require_once 'fragments/connection.php';
 
-                         $usr = $_SESSION['username'];
-                         echo $usr;
-
-                        $query = $pdo->prepare("
-                                      SELECT b.username AS sp_username, a.username AS cust_username, 
-                                      request_status, pet_service.service_name, start_servicing, end_servicing,  service_price 
-                                              FROM service_request 
-                                              INNER JOIN user_account AS b ON service_request.sp_id = b.account_id  
-                                              INNER JOIN user_account AS a ON service_request.account_id = a.account_id  
-                                              INNER JOIN pet_service ON service_request.service_id = pet_service.service_id 
-                                              WHERE request_status = 03 AND b.username = '$usr';");
-                        $query->execute();
-                        $result = $query->fetchAll();
-
-                        
-                        foreach($result as $query){
-                            echo "<tr>";
-                            echo "<td>" . $query['start_servicing'] . "</td>";
-                            echo "<td>" . $query['end_servicing'] . "</td>";
-                            echo "<td>" . $query['request_status'] . "</td>";
-                            echo "<td>" . $query['service_name'] . "</td>";
-                            echo "<td>" . $query['cust_username'] . "</td>";
-                            echo "</tr>";
-                        }
 
                         echo "</table>";
 
