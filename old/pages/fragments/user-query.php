b3ebf395a7a6878ad6a471fe1143aab896f51fc6
turquezakath <turquezakath@gmail.com>
2018-03-25 18:30:28 +0800

    Added modal for successful reset password.

--- old/pages/fragments/user-query.php
+++ old/pages/fragments/user-query.php
@@ -30,7 +30,17 @@ echo "<td>" . $query['name'] . "</td>";
 echo "<td>" . $query['address'] . "</td>";
 echo "<td>" . $query['username'] . "</td>";
 echo "<td>";
-echo '<a href="fragments/user-reset-password.php?id='.$query['accountNo'].'"><button class="btn btn-primary">Reset Password</button></a>';
+echo "<button class='btn btn-primary' data-toggle='modal' data-target='#myModal3'>Reset Password</button>
+      <div class='modal fade' data-toggle='modal' id='myModal3' role='dialog'>
+            <div class='modal-dialog'>
+                  <div class='modal-content'>
+                  <div class='modal-header' align='center'>
+                  <h4 class='modal-title'>Password has been reset.</h4>";
+                      echo '<a href="fragments/user-reset-password.php?id='.$query['accountNo'].'"><button class="btn btn-success">Okay</button></a>';
+echo             "</div>
+                  </div>
+            </div>  
+        </div>";
 echo "</td>";
 echo "<td>";
 	echo '<a href="fragments/account-hide.php?id='.$query['accountNo'].'"><button class="btn btn-success">Archive</button></a>';
