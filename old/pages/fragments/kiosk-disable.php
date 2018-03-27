5e32fa894c186d63c8ca8093e66756b7427e865c
CyreneJaneDispo <cyrenejanedispo132@gmail.com>
2018-03-27 23:42:49 +0800

    Alert box for disable button.

--- old/pages/fragments/kiosk-disable.php
+++ old/pages/fragments/kiosk-disable.php
@@ -12,6 +12,12 @@ $id=$_GET['id'];
 $query = $pdo->prepare("UPDATE kioskmachine SET kioskStatus='Disable' WHERE kioskId=$id");
 $query->execute();
 
-
-header("location:../kiosk-manage.php");
+if($query){
+        echo '<script type="text/javascript">
+              alert("Kiosk Disabled!");
+              location="../kiosk-manage.php";
+              </script>';
+    } else {
+        echo '<script>alert("Kiosk Disable Failed.")</script>';
+    }
 ?>
