687a5b07ae1261d3bbf215b8adc3af9f4dc409ce
CyreneJaneDispo <cyrenejanedispo132@gmail.com>
2018-03-28 00:51:51 +0800

    Alert box for sold button on unsold vouchers page.

--- admin module/pages/fragments/vouchers-sold.php
+++ admin module/pages/fragments/vouchers-sold.php
@@ -5,6 +5,7 @@
 * Select sold vouchers from the database
 * 
 * @author Alfa Leones
+* @author Cyrene Dispo
 */
 include 'connection.php';
 
@@ -13,6 +14,12 @@ $id=$_GET['id'];
 $query = $pdo->prepare("UPDATE vouchers SET voucherStatus='Sold' WHERE voucherId=$id");
 $query->execute();
 
-header("location:../vouchers-unsold.php");
-
+if($query){
+        echo '<script type="text/javascript">
+              alert("Voucher Sold!");
+              location="../vouchers-unsold.php";
+              </script>';
+    } else {
+        echo '<script>alert("Voucher Sold Failed.")</script>';
+    }
 ?>
