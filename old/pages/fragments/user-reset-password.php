9582bcf753160e4aed5af9fbe28b1418c8fc315f
turquezakath <turquezakath@gmail.com>
2018-03-27 20:46:47 +0800

    Added alert message.

--- old/pages/fragments/user-reset-password.php
+++ old/pages/fragments/user-reset-password.php
@@ -14,6 +14,13 @@ $id = $_GET['id'];
 $query = $pdo->prepare("UPDATE accounts SET password = '000' WHERE accountNo=$id");
 $query->execute();
 
-header("location:../view-staff-profile.php");
+    if($query){
+        echo '<script type="text/javascript">
+              alert("Password Reset Successful!");
+              location="../view-staff-profile.php";
+              </script>';
+    } else {
+        echo '<script>alert("Password Reset Failed.")</script>';
+    }
 
 ?>
