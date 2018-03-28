275094ae397222b120f6308e33c7b2bb847ffca2
turquezakath <turquezakath@gmail.com>
2018-03-29 02:00:20 +0800

    Updated header.

--- admin module/pages/fragments/page-head.php
+++ admin module/pages/fragments/page-head.php
@@ -1,27 +1,24 @@
 <!-- /. NAV TOP  -->
 <html>
   <head>
-    <link href="https://fonts.googleapis.com/css?family=Allura|Arima+Madurai|Cinzel+Decorative|Corben|Dancing+Script|Galindo|Gentium+Book+Basic|Great+Vibes|Henny+Penny|Indie+Flower|Kaushan+Script|Kurale|Life+Savers|Love+Ya+Like+A+Sister|Milonga|Miltonian+Tattoo|Niconne|Oregano|Original+Surfer|Pangolin|Parisienne|Philosopher|Princess+Sofia|Rancho|Risque|Salsa|Schoolbell|Special+Elite" rel="stylesheet">		
+    <link href="https://fonts.googleapis.com/css?family=Allura|Arima+Madurai|Cinzel+Decorative|Corben|Dancing+Script|Galindo|Gentium+Book+Basic|Great+Vibes|Henny+Penny|Indie+Flower|Kaushan+Script|Kurale|Life+Savers|Love+Ya+Like+A+Sister|Milonga|Miltonian+Tattoo|Niconne|Oregano|Original+Surfer|Pangolin|Parisienne|Philosopher|Princess+Sofia|Rancho|Risque|Salsa|Schoolbell|Special+Elite" rel="stylesheet">	
   </head>
-  <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0; position: sticky; width: 100%; top: 0; z-index:2;  background-color: #E59866;">
+    
+  <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0; position: sticky; width: 100%; top: 0; z-index:2">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-        <span class="sr-only">Toggle navigation
-        </span>
-        <span class="fa fa-bar">
-        </span>
-        <span class="fa fa-bar">
-        </span>
-        <span class="fa fa-bar">
-        </span>
-      </button>
-      <a class="navbar-brand" href="index.php"style ="font-family:audrey; background-color: #E59866"><font color="#145A32">WiFiRA ISP
+      <a class="navbar-brand" href="index.php"style ="font-family:life savers; background-color: #686667;">WiFiRA WISP
       </a>
     </div>
-    <div style="color: #145A32; padding: 15px 50px 5px 50px; float: right; font-size: 16px; font-family:pangolin;">Last Access: 
-      <?php echo date("F d, Y"); ?> &nbsp; 
-      <a href="logout.php" class="btn btn-danger square-btn-adjust" onclick="return confirm('Are you sure you want to log out?');">Logout
-      </a> 
+    <div style="color: white; padding: 15px 50px 5px 50px; float: right; font-size: 16px; font-family:pangolin;">
+      <?php 
+        echo "Welcome, " . $_SESSION['userAccount']->getUsername() . "!";
+      ?>
+      <div class="w3-bar" style="font-size:12px";>
+          <?php echo date("F d, Y"); ?>
+          <a href="logout.php" onclick="return confirm('Are you sure you want to log out?');"><button class="btn btn-danger btn-xs" style="float: right;">Logout</button>
+          </a>
+      </div>
     </div>
-  </nav>   
+  </nav> 
+
 </html>
