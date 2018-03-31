990e363db67b31ede3d7daa6397fe95637d9e31c
turquezakath <turquezakath@gmail.com>
2018-04-01 01:59:14 +0800

    Added print functionality.

--- adminmodule/pages/sales.php
+++ adminmodule/pages/sales.php
@@ -47,7 +47,7 @@ echo 'class="active-menu"';
                 <input type="date" name="d1" class="tcal" value="" placeholder="yyyy-mm-dd" style="height:29px;"/> To: 
                 <input type="date" name="d2" class="tcal" value="" placeholder="yyyy-mm-dd" style="height:29px;"/>
                 <button type="submit"><i class="fa fa-search" style=" margin-top:5px;margin-bottom: 5px;  "></i></button>
-                
+              </form>
               
               <form action="search-voucher.php" method="get" style="height:35px; ">
                 &nbsp;&nbsp; Voucher Search: <input type="text" name="s1" class="tcal" value="" placeholder="xxxxx-xxxxx" style="height:29px; "/> 
@@ -59,43 +59,72 @@ echo 'class="active-menu"';
                   <option value="">Choose Entity
                   </option>
                   <?php 
-require_once 'fragments/connection.php';
-$usersQuerry = $pdo->prepare("SELECT name FROM wifira.accounts  union SELECT kioskName FROM wifira.`kioskmachine`;");
-$usersQuerry->execute();
-$users = $usersQuerry->fetchAll();
-foreach ($users as $user){
-echo "<option>" . $user['name'] . "</option>";
-}
-?>
+                    require_once 'fragments/connection.php';
+                    $usersQuerry = $pdo->prepare("SELECT name FROM wifira.accounts  union SELECT kioskName FROM wifira.`kioskmachine`;");
+                    $usersQuerry->execute();
+                    $users = $usersQuerry->fetchAll();
+                    foreach ($users as $user){
+                    echo "<option>" . $user['name'] . "</option>";
+                    }
+                  ?>
                 </select>
-                <button type="submit"><i class="fa fa-search" style="margin-top:5px;margin-bottom: 5px;"></i></button>
-                
+                  <button type="submit"><i class="fa fa-search" style="margin-top:5px;margin-bottom: 5px;"></i></button>
               </form>
               
             </div>    
           </div>
+            
           <div class="jumbotron"> 
             <div style="float:right; margin-bottom: 15px;">
-          <a class="btn btn-success" href="#">
-            <i class="fa fa-file-text fa-lg" >
-            </i> Generate
-          </a> 
-          &nbsp;
-            <a class="btn btn-primary" href="sales-total.php"  ">
-              <i class="">
-              </i>Total Sales
-            </a>
-          </div>
+              <a class="btn btn-success" href="#null" onclick="printContent('print')">
+                <i class="fa fa-file-text fa-lg" >
+                </i> Generate
+              </a> &nbsp;
+              <a class="btn btn-primary" href="sales-total.php">
+                <i class="">
+                </i>Total Sales
+              </a>
+            </div>
+            <div id="print">
             <table class="table table-striped table-bordered table-hover" id="dataTables-example" name="anothercontent">
               <?php
-include 'fragments/sales-query.php';
-?>
+                include 'fragments/sales-query.php';
+              ?>
             </table>
+            </div>
           </div>
-          <!--  <input type="submit" name='submit' class="btn btn-warning" value="Print" class="col s6" class='submit' style="background-color:#686667; font-family:monospace; font-size:18px;"/><br />    -->
           
         </div>
       </div>
     </div>
   </body>
 </html>    
+
+<script type="text/javascript">
+function printContent(id){
+str=document.getElementById(id).innerHTML
+newwin=window.open('','printwin','left=100,top=100,width=400,height=400')
+newwin.document.write('<HTML>\n<HEAD>\n')
+newwin.document.write('<TITLE>Print Page</TITLE>\n')
+newwin.document.write('<script>\n')
+newwin.document.write('function chkstate(){\n')
+newwin.document.write('if(document.readyState=="complete"){\n')
+newwin.document.write('window.close()\n')
+newwin.document.write('}\n')
+newwin.document.write('else{\n')
+newwin.document.write('setTimeout("chkstate()",2000)\n')
+newwin.document.write('}\n')
+newwin.document.write('}\n')
+newwin.document.write('function print_win(){\n')
+newwin.document.write('window.print();\n')
+newwin.document.write('chkstate();\n')
+newwin.document.write('}\n')
+newwin.document.write('<\/script>\n')
+newwin.document.write('</HEAD>\n')
+newwin.document.write('<BODY onload="print_win()">\n')
+newwin.document.write(str)
+newwin.document.write('</BODY>\n')
+newwin.document.write('</HTML>\n')
+newwin.document.close()
+}
+</script>
